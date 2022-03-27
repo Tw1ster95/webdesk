e0ca9106be6390184879b3b7c357d36a64a11c6b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 19:06:10 +0300

    -

---
+++ inc/config_example.php
@@ -0,0 +1,10 @@
+<?php
+define('DIR', ('https://' . $_SERVER['SERVER_NAME']));
+
+define('MAIN_MYSQL_HOST', 'localhost');
+define('MAIN_MYSQL_USERNAME', 'root');
+define('MAIN_MYSQL_PASSWORD', '');
+define('MAIN_MYSQL_DB', 'webdesk');
+
+define('PHPMAILER_USERNAME', '');
+define('PHPMAILER_PASSWORD', '');
